96d67fb514111f58cb2e4b7b6cd5bb6ba6c95ce9
davesag <davesag@gmail.com>
2018-02-10 21:58:16 +0530

    n/a remove deployments for now as there is nothing to deploy

--- truffle.js
+++ truffle.js
@@ -23,21 +23,21 @@ module.exports = {
   license,
   authors: [author, ...contributors],
   networks: {
-    geth: { ...DEFAULT, gas: 999999 },
-    ropsten: {
-      network_id: 3,
-      provider: engineRopsten,
-      from: addresses[0],
-      gas: 4700000,
-      gasPrice: 222000000000
-    },
-    mainnet: {
-      network_id: 1,
-      provider: engineMainnet,
-      from: addresses[0],
-      gas: 5000000,
-      gasPrice: 75000000000
-    }
+    geth: { ...DEFAULT, gas: 999999 }
+    // ropsten: {
+    //   network_id: 3,
+    //   provider: engineRopsten,
+    //   from: addresses[0],
+    //   gas: 4700000,
+    //   gasPrice: 222000000000
+    // },
+    // mainnet: {
+    //   network_id: 1,
+    //   provider: engineMainnet,
+    //   from: addresses[0],
+    //   gas: 5000000,
+    //   gasPrice: 75000000000
+    // }
   },
   solc: {
     optimizer: {
